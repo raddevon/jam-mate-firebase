205c6c56a1f025585117ff2d70b094cc136dee2f
bradserlis <bradserlis@yahoo.com>
2018-08-06 23:57:50 -0700

    card vs list

--- components/Search.js
+++ components/Search.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, ImageBackground, Alert, Image, Platform, TouchableOpacity} from 'react-native';
-import { Container, Content, Header, Form, Input, Icon, Item, Button,
- Label, Left, Body, Right, Title, H3, H2, Grid, Col, Row, Footer, FooterTab, Thumbnail} from 'native-base';
+import { FlatList, Text, View, StyleSheet, ImageBackground, Alert, Image, Platform, TouchableOpacity} from 'react-native';
+import { Card, CardImage, CardItem, Container, Content, Header, Form, Input, Icon, Item, Button,
+ Label, Left, List, ListItem, Body, Right, Title, H3, H2, Grid, Col, Row, Footer, FooterTab, Thumbnail} from 'native-base';
  import FooterTabs from './Footer';
  import * as firebase from 'firebase';
  import * as Animatable from 'react-native-animatable';
 
 
+var instruments1 = {
+  instruments:[
+    'guitar',
+    'bass',
+    'drums'
+  ],
+  genres:[
+  'rock',
+  'metal',
+  'blues'
+  ],
+  };
 
 
 export default class Search extends Component{
@@ -40,13 +52,68 @@ export default class Search extends Component{
 
   render(){
     const { navigate } = this.props.navigation;
+    console.log('what is instruments1', instruments1)
 
     return(
       <Container>
       <Content>
       <H2> Search Page </H2>
+      <FlatList 
+              data={instruments1}
+              renderItem={({item, index}) => 
+            <Card>
+              <CardItem>
+                <Left>
+                <Thumbnail source={{uri: 'Image URL'}} />
+                <Body>
+                <List>
+                  <ListItem>
+                  <Text>{item.instruments}</Text>
+                  </ListItem>
+                </List>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent>
+                  <Icon active name="thumbs-up" />
+                  <Text>12 Likes</Text>
+                </Button>
+                </Left>
+                <Body>
+                <Button transparent>
+                  <Icon active name="chatbubbles" />
+                  <Text>4 Comments</Text>
+                </Button>
+                </Body>
+                <Right>
+                <Text>11h ago</Text>
+                </Right>
+                </CardItem>
+                </Card>          
+              }
+              keyExtractor={(item, index) => index}
+              >
+    </FlatList>
+
+                    <List>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={{ uri: 'Image URL' }} />
+              </Left>
+              <Body>
+                <Text>Kumar Pratik</Text>
+                <Text note>Doing what you like will always keep you happy . .</Text>
+              </Body>
+              <Right>
+                <Text note>3:43 pm</Text>
+              </Right>
+            </ListItem>
+          </List>
 
       </Content>
+
           <Footer>
           <FooterTab>
             <Button >
